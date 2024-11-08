591b65149e64d0d79830c8c484f915d9750cd579
tigeryfan <fanyixuanw@gmail.com>
2024-11-08 12:20:20 +0800

    Add error handling

--- extract_grades.py
+++ extract_grades.py
@@ -2,13 +2,18 @@ from bs4 import BeautifulSoup
 
 def extract_grades(html):
     soup = BeautifulSoup(html, "html.parser")
-    results = soup.find('div', class_='sidebar-items-list')
+    results = soup.find("div", class_="sidebar-items-list")
     grades = results.find_all("div", class_="cell")
-    overall_grade = grades[3].text.strip("\n").replace(" ", "").split("\n")
-    del overall_grade[-1]
-    overall_grade[1] = overall_grade[1].replace("(", "").replace(")", "")
+    grades = grades[3].text.split("\n")
+    del grades[0]
+    del grades[-1]
+    try:
+        grades[1] = grades[1].replace("(", "").replace(")", "")
+    except IndexError:
+        # gracefully handle classes with no grades
+        return None
     
-    return overall_grade
+    return grades
                 
 if __name__ == "__main__":
     with open("export-grades.html", "r") as f:
