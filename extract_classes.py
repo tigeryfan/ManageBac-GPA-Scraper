b95a70db90cdde595372d856868e611ecdcd1966
tigeryfan <fanyixuanw@gmail.com>
2024-11-08 11:15:58 +0800

    clean code to only use double quotes

--- extract_classes.py
+++ extract_classes.py
@@ -2,14 +2,14 @@ from bs4 import BeautifulSoup
 
 def extract_classes(html):
     soup = BeautifulSoup(html, "html.parser")
-    results = soup.find_all('li', class_='f-menu-submenu-item')
+    results = soup.find_all("li", class_="f-menu-submenu-item")
     classes = {}
     
     for li in results:
-        link = li.find('a', href=True)
-        if link and '/student/classes/' in link['href']:
-            class_id = link['href'].split('/')[-1]
-            class_name = link.find('span', class_='f-menu-submenu-link-title').text.strip()
+        link = li.find("a", href=True)
+        if link and "/student/classes/" in link["href"]:
+            class_id = link["href"].split("/")[-1]
+            class_name = link.find("span", class_="f-menu-submenu-link-title").text.strip()
             if class_id.isdigit():
                 classes[class_id] = class_name
                 
